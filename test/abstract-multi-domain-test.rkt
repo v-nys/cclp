061cf3066622855bd8c500eedb68ff036439bd95
Vincent Nys <vincentnys@gmail.com>
2016-08-09 14:11:02 +0200

    These tests pass.

--- test/abstract-multi-domain-test.rkt
+++ test/abstract-multi-domain-test.rkt
@@ -3,4 +3,11 @@
 (require "domain-boilerplate.rkt")
 (require "../src/abstract-multi-domain.rkt")
 
-; TODO test assemble-var-indices
+(check-equal? (assemble-var-indices g? (parse-term "α1")) (set))
+(check-equal? (assemble-var-indices a? (parse-term "γ1")) (set))
+(check-equal? (assemble-var-indices g? (parse-term "foo(bar)")) (set))
+(check-equal? (assemble-var-indices a? (parse-term "foo(bar)")) (set))
+(check-equal? (assemble-var-indices g? (parse-term "foo(bar(γ1,γ2,α3,α4))")) (set 1 2))
+(check-equal? (assemble-var-indices a? (parse-term "foo(bar(γ1,γ2,α3,α4))")) (set 3 4))
+(check-equal? (assemble-var-indices g? (parse-atom "foo(bar(γ1,γ2,α3,α4))")) (set 1 2))
+(check-equal? (assemble-var-indices a? (parse-atom "foo(bar(γ1,γ2,α3,α4))")) (set 3 4))
