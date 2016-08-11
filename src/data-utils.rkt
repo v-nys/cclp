a06f401fa2cfe0d40a0bbe3790aa2eaea1571c43
Vincent Nys <vincentnys@gmail.com>
2016-08-11 09:27:44 +0200

    Added data structure
    This allows consing with an empty list without changing the result type
    to a list.

--- src/data-utils.rkt
+++ src/data-utils.rkt
@@ -7,6 +7,10 @@
 (define-type (Opt a) (U none (some a)))
 (provide Opt)
 
+; 2-tuples are useful because consing with an empty list can change something's type to list when we want it to be a pair
+(struct (X Y) 2-tuple ([first : X] [second : Y]) #:transparent)
+(provide (struct-out 2-tuple))
+
 ; may want to create a set utils module?
 (: optional-set-union (All (A) (-> (Setof A) * (Setof A))))
 (define (optional-set-union . sets)
