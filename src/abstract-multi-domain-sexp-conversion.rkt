80805efbf41219c2e4be48c72e00d619b6a0eba9
Vincent Nys <vincentnys@gmail.com>
2016-09-21 17:00:55 +0200

    Planning to make sure functors and atom symbols are represented as symbols.

--- src/abstract-multi-domain-sexp-conversion.rkt
+++ src/abstract-multi-domain-sexp-conversion.rkt
@@ -1,7 +1,4 @@
 #lang racket
-(require rackunit)
-(require (prefix-in abp: "abstract-domain-boilerplate.rkt"))
-(require "../src/abstract-multi-domain-sexp-conversion.rkt")
 
 (define (domain-elem->sexp elem))
 (provide (contract-out [domain-elem->sexp (-> abstract-domain-elem? sexp?)]))
