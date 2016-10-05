06d9cc59efbb49fadb401691c0ef0967243207e1
Vincent Nys <vincentnys@gmail.com>
2016-10-05 07:29:35 +0200

    More updated tests.

--- test/abstraction-inspection-utils-test.rkt
+++ test/abstraction-inspection-utils-test.rkt
@@ -1,18 +1,20 @@
 #lang racket
 (require rackunit)
-(require "domain-boilerplate.rkt")
+(require "abstract-domain-boilerplate.rkt")
 (require "../src/abstract-multi-domain.rkt")
 (require "../src/abstraction-inspection-utils.rkt")
 
-(check-equal? (assemble-var-indices g? (parse-term "α1")) (set))
-(check-equal? (assemble-var-indices a? (parse-term "γ1")) (set))
-(check-equal? (assemble-var-indices g? (parse-term "foo(bar)")) (set))
+(check-equal? (assemble-var-indices g? (parse-abstract-term "α1")) (set))
+(check-equal? (assemble-var-indices a? (parse-abstract-term "γ1")) (set))
+(check-equal? (assemble-var-indices g? (parse-abstract-term "foo(bar)")) (set))
 
 (check-equal? (assemble-var-indices g? (abstract-function "bar" (list))) (set))
-(check-equal? (assemble-var-indices g? (abstract-function "foo" (list (abstract-function "bar" '())))) (set))
+(check-equal?
+ (assemble-var-indices g? (abstract-function "foo" (list (abstract-function "bar" '()))))
+ (set))
 
-(check-equal? (assemble-var-indices a? (parse-term "foo(bar)")) (set))
-(check-equal? (assemble-var-indices g? (parse-term "foo(bar(γ1,γ2,α3,α4))")) (set 1 2))
-(check-equal? (assemble-var-indices a? (parse-term "foo(bar(γ1,γ2,α3,α4))")) (set 3 4))
-(check-equal? (assemble-var-indices g? (parse-atom "foo(bar(γ1,γ2,α3,α4))")) (set 1 2))
-(check-equal? (assemble-var-indices a? (parse-atom "foo(bar(γ1,γ2,α3,α4))")) (set 3 4))
+(check-equal? (assemble-var-indices a? (parse-abstract-term "foo(bar)")) (set))
+(check-equal? (assemble-var-indices g? (parse-abstract-term "foo(bar(γ1,γ2,α3,α4))")) (set 1 2))
+(check-equal? (assemble-var-indices a? (parse-abstract-term "foo(bar(γ1,γ2,α3,α4))")) (set 3 4))
+(check-equal? (assemble-var-indices g? (parse-abstract-atom "foo(bar(γ1,γ2,α3,α4))")) (set 1 2))
+(check-equal? (assemble-var-indices a? (parse-abstract-atom "foo(bar(γ1,γ2,α3,α4))")) (set 3 4))
