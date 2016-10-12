68c8311197a74257e58fe54423c94d7480222c31
Vincent Nys <vincentnys@gmail.com>
2016-10-12 10:34:27 +0200

    Moved from regular provide to documented provide.

--- src/abstract-domain-ordering.rkt
+++ src/abstract-domain-ordering.rkt
@@ -1,4 +1,4 @@
-#lang racket
+#lang at-exp racket
 
 ; cannot define these functions in the domain module, would cause circular import between domain module and unification module
 (require "abstract-multi-domain.rkt")
@@ -7,12 +7,21 @@
 (require "abstract-substitution.rkt")
 (require "abstract-renaming.rkt")
 
+(require scribble/srcdoc)
+(require (for-doc scribble/manual))
+
 ; abstract domain elements are: abstract terms, abstract atoms
 (define (>=-extension domain-elem1 domain-elem2)
   (let* ([renamed-domain-elem2 (rename-apart domain-elem2 domain-elem1)]
          [unifier (abstract-unify (list (abstract-equality domain-elem1 renamed-domain-elem2)) 0)])
     (and (some? unifier) (equal? (apply-substitution (some-v unifier) domain-elem1) renamed-domain-elem2))))
-(provide (contract-out [>=-extension (-> abstract-domain-elem? abstract-domain-elem? boolean?)]))
+(provide
+ (proc-doc/names
+  >=-extension
+  (-> abstract-domain-elem? abstract-domain-elem? boolean?)
+  (domain-elem1 domain-elem2)
+  @{Checks whether abstract domain element @racket[domain-elem1] is at least as general as @racket[domain-elem2]}))
+
 
 (define (renames? domain-elem1 domain-elem2)
   (and (>=-extension domain-elem1 domain-elem2)
