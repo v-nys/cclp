e94d000e91b0c9effdaf15032e221e67462ba7d7
Vincent Nys <vincentnys@gmail.com>
2016-10-18 09:41:54 +0200

    Making good progress with tests for generational tree. Might be able to hit milestone tomorrow.

--- test/generational-tree-test.rkt
+++ test/generational-tree-test.rkt
@@ -38,6 +38,68 @@
 (define (node-display tree out)
   (print (node-label tree) out))
 
+(test-case
+ "the skeleton is computed correctly based on a branch of several nodes"
+ (define-values (atom0 atom1a atom1b atom1c atom2a atom2b atom2c atom2d atom3a atom3b atom3c atom4a atom4b atom4c atom4d)
+   (values (abp:parse-abstract-atom "primes(γ1,α1)")
+           
+           (abp:parse-abstract-atom "integers(γ2,α2)")
+           (abp:parse-abstract-atom "sift(α2,α1)")
+           (abp:parse-abstract-atom "length(α1,γ1)")
+           
+           (abp:parse-abstract-atom "plus(γ2,γ3,α3)")
+           (abp:parse-abstract-atom "integers(α3,α4)")
+           (abp:parse-abstract-atom "sift([γ2|α4],α1)")
+           (abp:parse-abstract-atom "length(α1,γ1)")
+           
+           (abp:parse-abstract-atom "integers(γ4,α4)")
+           (abp:parse-abstract-atom "sift([γ2|α4],α1)")
+           (abp:parse-abstract-atom "length(α1,γ1)")
+           
+           (abp:parse-abstract-atom "integers(γ4,α4)")
+           (abp:parse-abstract-atom "filter(γ2,α4,α5)")
+           (abp:parse-abstract-atom "sift(α5,[γ2|α6])")
+           (abp:parse-abstract-atom "length([γ2|α6],γ1)")))
+ (define-values (clause1 clause2 full-ai1 clause3)
+   (values (pre-abstract-rule (cbp:parse-rule "primes(X,Y) :- integers(2,Z), sift(Z,Y), length(Y,X)"))
+           (pre-abstract-rule (cbp:parse-rule "integers(N,[N|I]) :- plus(N,1,M), integers(M,I)"))
+           (full-evaluation (abp:parse-abstract-atom "plus(γ1,γ2,α2)") (abp:parse-abstract-atom "plus(γ1,γ2,γ3)"))
+           (pre-abstract-rule (cbp:parse-rule "sift([N|Ints],[N|Primes]) :- filter(N,Ints,F), sift(F,Primes)"))))
+ (define-values (branch-node1 branch-node2 branch-node3 branch-node4 branch-node5)
+   (values (resolution-info (list atom0) (some (cons 0 clause1)))
+           (resolution-info (list atom1a atom1b atom1c) (some (cons 0 clause2)))
+           (resolution-info (list atom2a atom2b atom2c atom2d) (some (cons 0 full-ai1)))
+           (resolution-info (list atom3a atom3b atom3c) (some (cons 1 clause3)))
+           (resolution-info (list atom4a atom4b atom4c atom4d) (none))))
+ (let* ([branch (list branch-node1 branch-node2 branch-node3 branch-node4 branch-node5)]
+        [expected4a (node atom4a (list))]
+        [expected4b (node atom4b (list))]
+        [expected4c (node atom4c (list))]
+        [expected4d (node atom4d (list))]
+        [expected3a (node atom3a (list expected4a))]
+        [expected3b (node atom3b (list expected4b expected4c))]
+        [expected3c (node atom3c (list expected4d))]
+        [expected2a (node atom2a (list))]
+        [expected2b (node atom2b (list expected3a))]
+        [expected2c (node atom2c (list expected3b))]
+        [expected2d (node atom2d (list expected3c))]
+        [expected1a (node atom1a (list expected2a expected2b))]
+        [expected1b (node atom1b (list expected2c))]
+        [expected1c (node atom1c (list expected2d))]
+        [expected0 (node atom0 (list expected1a expected1b expected1c))]
+        [actual (generational-tree-skeleton branch)])
+   (when (not (equal? actual (list expected0)))
+     (begin (displayln "actual:")
+            (tree-display (car actual) node-display)
+            (displayln "expected:")
+            (tree-display expected0 node-display)
+            (check-equal? actual expected0)))))
+
+; TODO test finding candidates
+; TODO test annotating for a specific target atom
+; TODO test annotating without a specific target atom
+
+; TODO reactivate when the rest has been tested
 ;(test-case
 ; "the generational tree is computed correctly based on a branch of several nodes"
 ; (define-values (atom0 atom1a atom1b atom1c atom2a atom2b atom2c atom2d atom3a atom3b atom3c atom4a atom4b atom4c atom4d)
@@ -89,13 +151,13 @@
 ;        [expected1c (node (atom-with-generation atom1c 0) (list expected2d))]
 ;        [expected0 (node (atom-with-generation atom0 0) (list expected1a expected1b expected1c))]
 ;        [actual (car (generational-trees branch))])
-;   
 ;   (when (not (equal? actual expected0))
 ;     (begin (displayln "actual:")
 ;            (tree-display actual node-display)
 ;            (displayln "expected:")
 ;            (tree-display expected0 node-display)
 ;            (check-equal? actual expected0)))))
+
 
 (let* ([a-atom (abstract-atom 'a '())]
        [b-atom (abstract-atom 'b '())]
