41639c71ea899ceefbb34063c7303bbb32a31ddb
Vincent Nys <vincentnys@gmail.com>
2016-10-26 14:04:44 +0200

    Thought of tricky corner case. Automatic depth-k widening should not be relied on...

--- src/depth-k-abstraction.rkt
+++ src/depth-k-abstraction.rkt
@@ -3,15 +3,20 @@
 (require (for-doc scribble/manual))
 
 (require "abstract-multi-domain.rkt")
+(require (only-in "abstraction-inspection-utils.rkt" maximum-var-index))
 
-(define (abstract elem k) elem)
+(define (abstract elem k)
+; TODO take into account maybe...
+  (define fresh-g (+ (maximum-var-index elem g?) 1))
+  (define fresh-a (+ (maximum-var-index elem a?) 1))
+  elem)
 (provide
  (proc-doc/names
   abstract
-  (-> abstract-domain-elem? exact-nonnegative-integer? abstract-domain-elem?)
+  (-> abstract-domain-elem? exact-positive-integer? abstract-domain-elem?)
   (abstract-domain-element k)
   @{Return the most specific abstraction of @racket[elem] whose depth is at most @racket[k].
- Depth is defined as follows: for abstract variables and constants in the abstract domain, it is 0.
+ Depth is defined as follows: for abstract variables and constants in the abstract domain, it is 1.
  For function terms, it is the depth of the argument with the greatest depth, increased by one.
  For abstract atoms and conjunctions, it is identical to the depth of the argument with the greatest depth.
  This definition is based on the assumption that we only use first-order logic and thus cannot nest atoms.}))
--- test/depth-k-abstract-test.rkt
+++ test/depth-k-abstract-test.rkt
@@ -4,20 +4,20 @@
 (require "../src/abstract-multi-domain.rkt")
 (require "../src/depth-k-abstraction.rkt")
 
-(check-equal? (abstract (a 1) 0) (a 1))
-(check-equal? (abstract (g 1) 0) (g 1))
-
 (check-equal? (abstract (a 1) 1) (a 1))
 (check-equal? (abstract (g 1) 1) (g 1))
 
+(check-equal? (abstract (a 1) 2) (a 1))
+(check-equal? (abstract (g 1) 2) (g 1))
+
 (check-equal?
- (abstract (abstract-atom 'foo (list (a 1))) 0)
+ (abstract (abstract-atom 'foo (list (a 1))) 1)
  (abstract-atom 'foo (list (a 1))))
 (check-equal?
- (abstract (abstract-atom 'foo (list (abstract-function 'bar '()))) 0)
+ (abstract (abstract-atom 'foo (list (abstract-function 'bar '()))) 1)
  (abstract-atom 'foo (list (abstract-function 'bar '()))))
 (check-equal?
- (abstract (abstract-atom 'foo (list (abstract-function 'bar (list (a 1) (a 2))))) 0)
+ (abstract (abstract-atom 'foo (list (abstract-function 'bar (list (a 1) (a 2))))) 1)
  (abstract-atom 'foo (list (a 3))))
 (check-equal?
  (abstract
@@ -29,10 +29,9 @@
      (list
       (abstract-function 'baz (list (a 1)))
       (abstract-function 'baz (list (a 1)))))))
-  1)
+  2)
  (abstract-atom 'foo (list (abstract-function 'bar (list (a 2) (a 2)))))
  "abstraction preserves aliasing within an atom")
-
 (let ([test-atom
        (abstract-atom
         'foo
@@ -43,8 +42,54 @@
            (abstract-function 'baz (list (a 1)))
            (abstract-function 'baz (list (a 1)))))))])
   (check-equal?
-   (abstract (list test-atom test-atom) 1)
+   (abstract (list test-atom test-atom) 2)
    (list
     (abstract-atom 'foo (list (abstract-function 'bar (list (a 2) (a 2)))))
     (abstract-atom 'foo (list (abstract-function 'bar (list (a 2) (a 2))))))
    "abstraction preserves aliasing within an abstract conjunction"))
+(check-equal?
+ (abstract
+  (abstract-function ; depth is 5 - occurs at level 1
+   'foo
+   (list
+    (abstract-function ; depth is 2 - occurs at level 2
+     'bar
+     (list
+      (g 1) ; depth is 1
+      (a 2))) ; depth is 1
+    (abstract-function ; depth is 4 - occurs at level 2
+     'cons
+     (list
+      (g 3)
+      (abstract-function ; depth is 3 - occurs at level 3
+       'cons
+       (list
+        (abstract-function ; depth is 2 - occurs at level 4
+         'bar
+         (list
+          (g 1) ; depth is 1 - occurs at level 5
+          (a 2))) ; depth is 1 - occurs at level 5
+        (abstract-function
+         'nil ; depth is 1 - occurs at level 5
+         '())))))))
+  4)
+ (abstract-function ; depth is at most 4
+   'foo
+   (list ; elements have depth at most 3
+    (abstract-function
+     'bar
+     (list ; elements have depth at most 2
+      (g 1)
+      (a 2)))
+    (abstract-function
+     'cons
+     (list ; elements have depth at most 2
+      (g 3)
+      (abstract-function
+       'cons
+       (list ; elements have depth 1
+        (a 3)
+        (abstract-function
+         'nil
+         '())))))))
+ "aliasing can be lost if aliased terms occur at different levels")
