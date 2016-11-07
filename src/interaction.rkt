d02a49798a75de3f821312f1f6d9df209c3e998b
Vincent Nys <vincentnys@gmail.com>
2016-11-07 17:35:10 +0100

    Added auxiliary function for checking s-similarity. May want to add tests, but quick check on primes looks good.

--- src/interaction.rkt
+++ src/interaction.rkt
@@ -309,9 +309,28 @@
   (define node-index-1 (prompt-for-integer))
   (displayln "Please supply the index of the second node.")
   (define node-index-2 (prompt-for-integer))
-  ; TODO find branch ending in the more recent node
-  ; check if the older one is on the same branch
-  (void))
+  (define
+    branch
+    (shortest-branch-containing-indices (list node-index-1 node-index-2) t))
+  (if branch
+      (void)
+      (displayln "Could not find a branch with bost indices (in the correct order)")))
+
+(define (shortest-branch-containing-indices indices t)
+  (match indices
+    [(list) (list)]
+    [(list-rest i1 is)
+     (let* ([t-label (node-label t)]
+            [t-index (cond [(tree-label? t-label) (tree-label-index t-label)]
+                           [(widening? t-label) (widening-index t-label)]
+                           [else #f])])
+       (cond [(not t-index) #f] ; as nodes without index do not have descendants with indices
+             [(equal? i1 t-index)
+              ; TODO: next two long expressions are nearly identical
+              ; can I consolidate them?
+              (let ([child-result (foldl (λ (el acc) (if acc acc (shortest-branch-containing-indices is el))) #f (node-children t))]) (if child-result (cons t-label child-result) #f))]
+             [else (let ([child-result (foldl (λ (el acc) (if acc acc (shortest-branch-containing-indices (cons i1 is) el))) #f (node-children t))]) (if child-result (cons t-label child-result) #f))]))]
+    [_ #f]))
 
 (define (largest-node-index t)
   (match t
