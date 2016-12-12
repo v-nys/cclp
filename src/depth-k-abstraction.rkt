4cf42ff7b2268d0d7d8f42225fa63c9412db59b9
Vincent Nys <vincentnys@gmail.com>
2016-12-12 20:57:06 +0100

    Tests to submodule...

--- src/depth-k-abstraction.rkt
+++ src/depth-k-abstraction.rkt
@@ -96,3 +96,93 @@
  For function terms, it is the depth of the argument with the greatest depth, increased by one.
  For abstract atoms and conjunctions, it is identical to the depth of the argument with the greatest depth.
  This definition is based on the assumption that we only use first-order logic and thus cannot nest atoms.}))
+
+(module+ test
+  (require rackunit)
+  (check-equal? (abstract (a 1) 1) (a 1))
+  (check-equal? (abstract (g 1) 1) (g 1))
+  (check-equal? (abstract (a 1) 2) (a 1))
+  (check-equal? (abstract (g 1) 2) (g 1))
+  (check-equal?
+   (abstract (abstract-atom 'foo (list (a 1))) 1)
+   (abstract-atom 'foo (list (a 1))))
+  (check-equal?
+   (abstract (abstract-atom 'foo (list (abstract-function 'bar '()))) 1)
+   (abstract-atom 'foo (list (abstract-function 'bar '()))))
+  (check-equal?
+   (abstract (abstract-atom 'foo (list (abstract-function 'bar (list (a 1) (a 2))))) 1)
+   (abstract-atom 'foo (list (a 3))))
+  (check-equal?
+   (abstract
+    (abstract-atom
+     'foo
+     (list
+      (abstract-function
+       'bar
+       (list
+        (abstract-function 'baz (list (a 1)))
+        (abstract-function 'baz (list (a 1)))))))
+    2)
+   (abstract-atom 'foo (list (abstract-function 'bar (list (a 2) (a 2)))))
+   "abstraction preserves aliasing within an atom")
+  (let ([test-atom
+         (abstract-atom
+          'foo
+          (list
+           (abstract-function
+            'bar
+            (list
+             (abstract-function 'baz (list (a 1)))
+             (abstract-function 'baz (list (a 1)))))))])
+    (check-equal?
+     (abstract (list test-atom test-atom) 2)
+     (list
+      (abstract-atom 'foo (list (abstract-function 'bar (list (a 2) (a 2)))))
+      (abstract-atom 'foo (list (abstract-function 'bar (list (a 2) (a 2))))))
+     "abstraction preserves aliasing within an abstract conjunction"))
+  (check-equal?
+   (abstract
+    (abstract-function ; depth is 5 - occurs at level 1
+     'foo
+     (list
+      (abstract-function ; depth is 2 - occurs at level 2
+       'bar
+       (list
+        (g 1) ; depth is 1
+        (a 2))) ; depth is 1
+      (abstract-function ; depth is 4 - occurs at level 2
+       'cons
+       (list
+        (g 3)
+        (abstract-function ; depth is 3 - occurs at level 3
+         'cons
+         (list
+          (abstract-function ; depth is 2 - occurs at level 4
+           'bar
+           (list
+            (g 1) ; depth is 1 - occurs at level 5
+            (a 2))) ; depth is 1 - occurs at level 5
+          (abstract-function
+           'nil ; depth is 1 - occurs at level 5
+           '())))))))
+    4)
+   (abstract-function ; depth is at most 4
+    'foo
+    (list ; elements have depth at most 3
+     (abstract-function
+      'bar
+      (list ; elements have depth at most 2
+       (g 1)
+       (a 2)))
+     (abstract-function
+      'cons
+      (list ; elements have depth at most 2
+       (g 3)
+       (abstract-function
+        'cons
+        (list ; elements have depth 1
+         (a 3)
+         (abstract-function
+          'nil
+          '())))))))
+   "aliasing can be lost if aliased terms occur at different levels"))
