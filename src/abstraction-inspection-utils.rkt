13b23180dc1b4bf41ad79f2e85acf3411ea16d6a
Vincent Nys <vincentnys@gmail.com>
2016-12-13 10:35:35 +0100

    Moved more tests.

--- src/abstraction-inspection-utils.rkt
+++ src/abstraction-inspection-utils.rkt
@@ -97,3 +97,54 @@
   (-> abstract-domain-elem? abstract-term? boolean?)
   (abstraction subterm)
   @{Checks whether @racket[subterm] occurs anywhere in @racket[abstraction].}))
+
+(module+ test
+  (require rackunit)
+  (require "cclp-interpreter.rkt")
+  (check-equal? (assemble-var-indices g? (interpret-abstract-term "α1")) (set))
+  (check-equal? (assemble-var-indices a? (interpret-abstract-term "γ1")) (set))
+  (check-equal? (assemble-var-indices g? (interpret-abstract-term "foo(bar)")) (set))
+
+  (check-equal? (assemble-var-indices g? (abstract-function "bar" (list))) (set))
+  (check-equal?
+   (assemble-var-indices g? (abstract-function "foo" (list (abstract-function "bar" '()))))
+   (set))
+
+  (check-equal? (assemble-var-indices a? (interpret-abstract-term "foo(bar)")) (set))
+  (check-equal? (assemble-var-indices g? (interpret-abstract-term "foo(bar(γ1,γ2,α3,α4))")) (set 1 2))
+  (check-equal? (assemble-var-indices a? (interpret-abstract-term "foo(bar(γ1,γ2,α3,α4))")) (set 3 4))
+  (check-equal? (assemble-var-indices g? (interpret-abstract-atom "foo(bar(γ1,γ2,α3,α4))")) (set 1 2))
+  (check-equal? (assemble-var-indices a? (interpret-abstract-atom "foo(bar(γ1,γ2,α3,α4))")) (set 3 4))
+
+  (check-equal?
+   (assemble-var-indices
+    g?
+    (list
+     (interpret-abstract-atom "foo(bar(γ1,γ2,α3,α4))")
+     (interpret-abstract-atom "foo(bar(γ5,γ6,α7,α8))")))
+   (set 1 2 5 6))
+  (check-equal?
+   (assemble-var-indices
+    a?
+    (list
+     (interpret-abstract-atom "foo(bar(γ1,γ2,α3,α4))")
+     (interpret-abstract-atom "foo(bar(γ5,γ6,α7,α8))")))
+   (set 3 4 7 8))
+
+  (check-equal?
+   (assemble-var-indices
+    a?
+    (full-evaluation (interpret-abstract-atom "del(α1,[γ1|γ2],α2)")
+                     (interpret-abstract-atom "del(γ3,[γ1|γ2],γ4)")))
+   (set 1 2))
+  (check-equal?
+   (assemble-var-indices
+    g?
+    (full-evaluation (interpret-abstract-atom "del(α1,[γ1|γ2],α2)")
+                     (interpret-abstract-atom "del(γ3,[γ1|γ2],γ4)")))
+   (set 1 2 3 4))
+
+  (check-equal?
+   (contains-subterm? (interpret-abstract-conjunction "bar(α2),foo(q(γ7),α1)") (g 7)) #t)
+  (check-equal?
+   (contains-subterm? (interpret-abstract-conjunction "bar(α2),foo(q(γ7),α1)") (g 6)) #f))
