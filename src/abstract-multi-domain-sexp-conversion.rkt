f1ff343706367586b28b7036284be9130b53be30
Vincent Nys <vincentnys@gmail.com>
2016-12-14 13:13:33 +0100

    Moved more code.

--- src/abstract-multi-domain-sexp-conversion.rkt
+++ src/abstract-multi-domain-sexp-conversion.rkt
@@ -26,3 +26,25 @@
   (abstract-atom (car sexp)
                  (map sexp->abstract-term (cdr sexp))))
 (provide (contract-out [sexp->abstract-atom (-> list? abstract-atom?)]))
+
+(module+ test
+  (require rackunit "cclp-interpreter.rkt")
+  (check-equal? (abstract-domain-elem->sexp (interpret-abstract-atom "b"))
+                '(b))
+  (check-equal? (abstract-domain-elem->sexp (interpret-abstract-atom "integers(γ1,α1)"))
+                '(integers (γ sym1) (α sym1)))
+  (check-equal? (abstract-domain-elem->sexp
+                 (interpret-abstract-atom "atom_containing_list([γ1,γ2|γ3])"))
+                '(atom_containing_list (cons (γ sym1) (cons (γ sym2) (γ sym3)))))
+  (check-equal? (abstract-domain-elem->sexp (interpret-abstract-atom "atom_containing_list([])"))
+                '(atom_containing_list (nil)))
+  (check-equal? (sexp->abstract-atom '(integers (γ sym1) (α sym1)))
+                (interpret-abstract-atom "integers(γ1,α1)"))
+  (check-equal? (sexp->abstract-atom '(a))
+                (interpret-abstract-atom "a"))
+
+  (check-equal? (abstract-domain-elem->sexp (list)) '())
+  (check-equal?
+   (abstract-domain-elem->sexp
+    (list (interpret-abstract-atom "a") (interpret-abstract-atom "b") (interpret-abstract-atom "c")))
+   '(cons (a) (cons (b) (cons (c) ())))))
