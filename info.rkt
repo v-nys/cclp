8cd740ec17bf6a647d12cffab77ba0d8caa76c51
Vincent Nys <vincentnys@gmail.com>
2017-01-19 11:00:34 +0100

    Forgot to run tests. Showed an issue with info.rkt.

--- info.rkt
+++ info.rkt
@@ -2,7 +2,7 @@
 (define scribblings '(("scribblings/cclp.scrbl" ())))
 (define collection "cclp")
 (define version "0.1")
-; TODO add dependencies, as per http://beautifulracket.com/jsonic-3/the-info.rkt-file.html(define deps '("base"
+(define deps '("base"
                "beautiful-racket-lib"
                "brag"
                "graph"
