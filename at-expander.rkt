ddaf13c4b531d4d9c05aaf44ff8f8ec68644aa78
Vincent Nys <vincentnys@gmail.com>
2017-01-24 13:54:55 +0100

    Not done with expander yet, but making progress.

--- at-expander.rkt
+++ at-expander.rkt
@@ -1,14 +1,77 @@
 #lang br
 (require (for-syntax syntax/parse))
+(require "abstract-multi-domain.rkt")
+(require "abstract-analysis.rkt")
+(require racket-tree-utils/src/tree)
 
 (define-syntax (at-module-begin stx)
   (syntax-parse stx
     [(_ _PARSE-TREE ...)
-     (syntax/loc stx (#%module-begin
-        (define at _PARSE-TREE ...)))]))
+     (syntax/loc stx (#%module-begin '_PARSE-TREE ...))]))
 (provide (rename-out [at-module-begin #%module-begin]) #%top-interaction)
 
 (define-syntax (top stx)
   (syntax-parse stx
-    [_ #'3]))
+    [(_ WS-STX ... AT-STX)
+     (syntax/loc stx AT-STX)]))
 (provide top)
+
+(define-syntax (at stx)
+  (syntax-parse stx
+    [(_ "(" LABEL-STACK-OPT-ORIGIN-STX WS-PREFIXED-SUBTREES-STX ")")
+     (syntax/loc stx
+       (node
+        LABEL-STACK-OPT-ORIGIN-STX
+        WS-PREFIXED-SUBTREES-STX))]))
+
+(define-syntax (label-stack-opt-origin stx)
+  (syntax-parse stx
+    ; TODO case with origin
+    [(LABEL-STX _WS-STX-1 STACK-STX _WS-STX-2)
+     (syntax/loc stx
+       (struct-copy tree-label LABEL-STX [preprior-stack STACK-STX]))]))
+
+(define-syntax (at-label stx)
+  (syntax-parse stx
+    [(_ NUM-STX "." ACON-P-SEL-STX)
+     (syntax/loc stx
+       (tree-label (car ACON-P-SEL-STX) (cdr ACON-P-SEL-STX) (list) #f NUM-STX (list)))]))
+
+(define-syntax (acon-with-potential-selection stx)
+  (syntax-parse stx
+    [(_ NESTED-STX) (syntax/loc stx NESTED-STX)]))
+
+(define-syntax (acon-with-selection stx)
+  (syntax-parse stx
+    [(_ "*" SELECTED-ATOM-STX "*")
+     (syntax/loc stx SELECTED-ATOM-STX)]))
+
+(define-syntax (abstract-atom stx)
+  (syntax-parse stx
+    [(_ NESTED-STX)
+     (syntax/loc stx NESTED-STX)]))
+(provide abstract-atom)
+
+
+
+;graph-stack : OPEN-RECTANGULAR-PAREN [graph (opt-ws AMPERSAND opt-ws graph)*] CLOSE-RECTANGULAR-PAREN
+;graph : QUESTION-MARK | precedence (COMMA opt-ws precedence)*
+;precedence : abstract-atom opt-ws GT opt-ws abstract-atom
+
+(define-syntax (graph-stack stx)
+  (syntax-parse stx
+    [("[" "]")
+     (syntax/loc stx (list))]
+    [("[" GRAPH-STX "]")
+     ; TODO deal with this
+     (syntax/loc stx (list))]
+    [("[" GRAPH-STX _OPT-WS-1-STX "&" _OPT-WS-2-STX REST-GRAPH-STX ... "]")
+     ; TODO deal with this
+     (syntax/loc stx (list))]))
+
+;(define-syntax (graph stx)
+;  (syntax-parse stx
+;    [("?") (syntax/loc stx (mk-preprior-model))]))
+
+; TODO
+(define-syntax (ws-prefixed-subtrees stx) (syntax/loc stx #'(list)))
--- at-parser.rkt
+++ at-parser.rkt
@@ -1,40 +1,45 @@
 #lang brag
 
-top : WS* at
-at : OPEN-PAREN at-label WS+ graph-stack (WS+ substitution WS+ knowledge WS+ at)* CLOSE-PAREN
+top : opt-ws at
+at : OPEN-PAREN label-stack-opt-origin ws-prefixed-subtrees CLOSE-PAREN
+label-stack-opt-origin : at-label mandatory-ws graph-stack mandatory-ws opt-origin
+mandatory-ws : WS+
+opt-origin : [substitution mandatory-ws knowledge]
+ws-prefixed-subtrees : [mandatory-ws at (mandatory-ws at)*]
+opt-ws : WS*
 
 at-label : [NUMBER PERIOD] acon-with-potential-selection
 acon-with-potential-selection : acon-with-selection | acon-without-selection
-acon-with-selection : [nonempty-acon-without-selection COMMA WS*] ASTERISK abstract-atom ASTERISK [COMMA WS* nonempty-acon-without-selection]
-nonempty-acon-without-selection : abstract-atom (COMMA WS* abstract-atom)*
+acon-with-selection : [nonempty-acon-without-selection COMMA opt-ws] ASTERISK abstract-atom ASTERISK [COMMA opt-ws nonempty-acon-without-selection]
+nonempty-acon-without-selection : abstract-atom (COMMA opt-ws abstract-atom)*
 abstract-atom : abstract-atom-with-args | abstract-atom-without-args
-abstract-atom-with-args : SYMBOL OPEN-PAREN abstract-term (COMMA WS* abstract-term)* CLOSE-PAREN
+abstract-atom-with-args : SYMBOL OPEN-PAREN abstract-term (COMMA opt-ws abstract-term)* CLOSE-PAREN
 abstract-term : abstract-variable | abstract-function-term | abstract-lplist
 abstract-variable : SYMBOL NUMBER
-abstract-function-term : (SYMBOL [OPEN-PAREN abstract-term (COMMA WS* abstract-term)* CLOSE-PAREN]) | abstract-number-term
+abstract-function-term : (SYMBOL [OPEN-PAREN abstract-term (COMMA opt-ws abstract-term)* CLOSE-PAREN]) | abstract-number-term
 abstract-number-term : abstract-number
 abstract-number : NUMBER
-abstract-lplist : OPEN-RECTANGULAR-PAREN [abstract-term (COMMA WS* abstract-term)* [LIST-SEPARATOR (abstract-lplist | abstract-variable)]] CLOSE-RECTANGULAR-PAREN
+abstract-lplist : OPEN-RECTANGULAR-PAREN [abstract-term (COMMA opt-ws abstract-term)* [LIST-SEPARATOR (abstract-lplist | abstract-variable)]] CLOSE-RECTANGULAR-PAREN
 abstract-atom-without-args : SYMBOL
 acon-without-selection : EMPTY-GOAL | nonempty-acon-without-selection
 
-graph-stack : OPEN-RECTANGULAR-PAREN [graph (WS* AMPERSAND WS* graph)*] CLOSE-RECTANGULAR-PAREN
-graph : QUESTION-MARK | precedence (COMMA WS* precedence)*
-precedence : abstract-atom WS* GT WS* abstract-atom
+graph-stack : OPEN-RECTANGULAR-PAREN [graph (opt-ws AMPERSAND opt-ws graph)*] CLOSE-RECTANGULAR-PAREN
+graph : QUESTION-MARK | precedence (COMMA opt-ws precedence)*
+precedence : abstract-atom opt-ws GT opt-ws abstract-atom
 
-substitution : OPEN-CURLY-PAREN [substitution-pair (COMMA WS* substitution-pair)*] CLOSE-CURLY-PAREN
+substitution : OPEN-CURLY-PAREN [substitution-pair (COMMA opt-ws substitution-pair)*] CLOSE-CURLY-PAREN
 substitution-pair : abstract-variable SLASH abstract-term
 
 knowledge : (rule | fullai-rule) PERIOD
-rule : (atom WS* IMPLIES WS* conjunction) | atom
-atom : SYMBOL [OPEN-PAREN term (COMMA WS* term)* CLOSE-PAREN]
+rule : (atom opt-ws IMPLIES opt-ws conjunction) | atom
+atom : SYMBOL [OPEN-PAREN term (COMMA opt-ws term)* CLOSE-PAREN]
 term : variable | function-term | lplist
 variable : VARIABLE-IDENTIFIER
-function-term : (SYMBOL [OPEN-PAREN term (COMMA WS* term)* CLOSE-PAREN]) | number-term
+function-term : (SYMBOL [OPEN-PAREN term (COMMA opt-ws term)* CLOSE-PAREN]) | number-term
 number-term : NUMBER
-lplist : OPEN-RECTANGULAR-PAREN [term (COMMA WS* term)* [LIST-SEPARATOR (lplist | variable)]] CLOSE-RECTANGULAR-PAREN
-conjunction : atom (COMMA WS* atom)*
+lplist : OPEN-RECTANGULAR-PAREN [term (COMMA opt-ws term)* [LIST-SEPARATOR (lplist | variable)]] CLOSE-RECTANGULAR-PAREN
+conjunction : atom (COMMA opt-ws atom)*
 
 fullai-rule : fullai-rule-with-body | fullai-rule-without-body
-fullai-rule-with-body : abstract-atom-with-args WS* LEADS-TO WS* substitution
+fullai-rule-with-body : abstract-atom-with-args opt-ws LEADS-TO opt-ws substitution
 fullai-rule-without-body : abstract-atom-with-args
