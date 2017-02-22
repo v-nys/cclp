675b60812a66c7d66130875ab68f12c88cf8b25a
Vincent Nys <vincentnys@gmail.com>
2017-02-22 16:58:03 +0100

    Moved structs used for representation of generational tree to separate file.

--- generational-graph.rkt
+++ generational-graph.rkt
@@ -23,7 +23,6 @@
 #lang at-exp racket
 
 (require
-  racket/struct
   scribble/srcdoc
   graph
   racket-list-utils/utils
@@ -37,82 +36,10 @@
   (prefix-in slbranch: "analysis-trees/sameleaves-branch.rkt")
   (prefix-in ck: "concrete-knowledge.rkt")
   "data-utils.rkt"
-  (only-in "control-flow.rkt" aif it))
+  (only-in "control-flow.rkt" aif it)
+  "gen-graph-structs.rkt")
 
 (require (for-doc scribble/manual))
-
-
-(struct generation (number origin)
-  #:methods
-  gen:custom-write
-  [(define write-proc
-     (make-constructor-style-printer
-      (λ (obj) 'generation)
-      (λ (obj) (list (generation-number obj)
-                     (generation-origin obj)))))]
-  #:methods
-  gen:equal+hash
-  [(define (equal-proc g1 g2 equal?-recur)
-     (and (equal?-recur (generation-number g1)
-                        (generation-number g2))
-          (equal?-recur (generation-origin g1)
-                        (generation-origin g2))))
-   (define (hash-proc my-gen hash-recur)
-     (+ (hash-recur (generation-number my-gen))
-        (hash-recur (generation-origin my-gen))))
-   (define (hash2-proc my-gen hash-recur)
-     (+ (hash-recur (generation-number my-gen))
-        (hash-recur (generation-origin my-gen))))])
-(provide
- (struct*-doc
-  generation
-  ([number (or/c exact-positive-integer? (cons/c symbol? exact-integer?))]
-   [origin (or/c #f exact-positive-integer?)])
-  @{Used to track the recursion depth of an atom with respect to a uniquely identified target atom.}))
-
-(struct identified-abstract-conjunct-with-gen-range (id-conjunct range))
-(provide (struct-out identified-abstract-conjunct-with-gen-range))
-
-(struct identified-abstract-conjunct (conjunct id-number))
-(provide (struct-out identified-abstract-conjunct))
-
-(struct gen-range (low high origin))
-(provide (struct-out gen-range))
-
-(struct symsum (sym num))
-(provide (struct-out symsum))
-
-(struct identified-atom-with-generation (id-atom generation)
-  #:methods
-  gen:custom-write
-  [(define write-proc
-     (make-constructor-style-printer
-      (λ (obj) 'identified-atom-with-generation)
-      (λ (obj) (list (identified-atom-with-generation-id-atom obj)
-                     (identified-atom-with-generation-generation obj)))))]
-  #:methods
-  gen:equal+hash
-  [(define (equal-proc a1 a2 equal?-recur)
-     (and (equal?-recur (identified-atom-with-generation-id-atom a1)
-                        (identified-atom-with-generation-id-atom a2))
-          (equal?-recur (identified-atom-with-generation-generation a1)
-                        (identified-atom-with-generation-generation a2))))
-   (define (hash-proc my-awg hash-recur)
-     (+ (hash-recur (identified-atom-with-generation-id-atom my-awg))
-        (hash-recur (identified-atom-with-generation-generation my-awg))))
-   (define (hash2-proc my-awg hash2-recur)
-     (+ (hash2-recur (identified-atom-with-generation-id-atom my-awg))
-        (hash2-recur (identified-atom-with-generation-generation my-awg))))])
-(provide
- (struct*-doc
-  identified-atom-with-generation
-  ([id-atom identified-atom?]
-   [generation generation?])
-  @{The label of an atom vertex in a generational graph.
-     The field @racket[atom] is a single identified atom which,
-     juxtaposed with other @racket[identified-atom]s at the same depth,
-     forms an abstract conjunction encountered during analysis.
-     The field @racket[generation] indicates how many recursive unfoldings of the target atom took place before @racket[atom] was introduced.}))
 
 (define (knowledge-output-length knowledge)
   (match knowledge
@@ -124,35 +51,6 @@
   (-> (or/c ck:rule? full-evaluation?) exact-nonnegative-integer?)
   (knowledge)
   @{Computes how many conjuncts will be introduced when @racket[knowledge] is applied.}))
-
-(struct identified-atom (atom uid)
-  #:methods
-  gen:custom-write
-  [(define write-proc
-     (make-constructor-style-printer
-      (λ (obj) 'identified-atom)
-      (λ (obj) (list (identified-atom-atom obj)
-                     (identified-atom-uid obj)))))]
-  #:methods
-  gen:equal+hash
-  [(define (equal-proc a1 a2 equal?-recur)
-     (and (equal?-recur (identified-atom-atom a1)
-                        (identified-atom-atom a2))
-          (equal?-recur (identified-atom-uid a1)
-                        (identified-atom-uid a2))))
-   (define (hash-proc my-ida hash-recur)
-     (+ (hash-recur (identified-atom-atom my-ida))
-        (hash-recur (identified-atom-uid my-ida))))
-   (define (hash2-proc my-ida hash2-recur)
-     (+ (hash2-recur (identified-atom-atom my-ida))
-        (hash2-recur (identified-atom-uid my-ida))))])
-(provide
- (struct*-doc
-  identified-atom
-  ([atom abstract-atom?]
-   [uid exact-nonnegative-integer?])
-  @{A uniquely identifiable instance of an abstract atom in a generational graph (or skeleton).
-     The field @racket[uid] is unique to each atom in a generational graph.}))
 
 (struct index-range (start end-before))
 (define (contains range idx)
