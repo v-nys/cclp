c00e2270d451134b7be286c9b645d3b084f8f4ba
Vincent Nys <vincentnys@gmail.com>
2017-02-23 14:41:48 +0100

    Added abstracted index ranges to data structures. Now to modify the DSL and the structs so that modifications can be used.

--- analysis-trees/sameleaves-multi-branch.rkt
+++ analysis-trees/sameleaves-multi-branch.rkt
@@ -8,4 +8,4 @@
     {g3/t(g5,g6)} collect(t(X,Y),L) :- collect(X,XL),collect(Y,YL),append(XL,YL,L).
     (.5.collect(g7,a7),collect(g8,a8),append(a7,a8,a5),collect(g6,a6),append(a5,a6,a3),collect(g4,a4),append(a3,a4,a1),collect(g2,a2),eq(a1,a2)
      {g5/t(g7,g8)} collect(t(X,Y),L) :- collect(X,XL),collect(Y,YL),append(XL,YL,L).
-     (.!GEN collect(g7,a7),collect(g8,a8),append(a7,a8,a5),multi((collect(g<1,i,1>,a<1,i,1>),append(a<1,i,2>,a<1,i,1>,a<1,i,3>)),#f,{g<1,1,1>/g6,a<1,1,1>/a6,a<1,1,2>/a5,a<1,1,3>/a3},{a<1,i+1,2>/a<1,i,3>},{g<1,L,1>/g4,a<1,L,1>/a4,a<1,L,2>/a3,a<1,L,3>/a1}),collect(g2,a2),eq(a1,a2)))))))
+     (.!GEN collect(g7,a7),collect(g8,a8),append(a7,a8,a5),multi((collect(g<1,i,1>,a<1,i,1>),append(a<1,i,2>,a<1,i,1>,a<1,i,3>)),#f,{g<1,1,1>/g6,a<1,1,1>/a6,a<1,1,2>/a5,a<1,1,3>/a3},{a<1,i+1,2>/a<1,i,3>},{g<1,L,1>/g4,a<1,L,1>/a4,a<1,L,2>/a3,a<1,L,3>/a1}),collect(g2,a2),eq(a1,a2) ((3 6))))))))
