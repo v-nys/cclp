11d036b894592fedecc671207d36654c7f8fac26
Vincent Nys <vincentnys@gmail.com>
2017-06-22 17:00:16 +0200

    So far, so good for summarizing the analysis. Should test with the larger analyses, too, but will need to rerun them.

--- mi-map.rkt
+++ mi-map.rkt
@@ -1,11 +1,15 @@
 #lang racket
 (require racket-tree-utils/src/tree)
 (require "abstract-analysis.rkt")
+(require (prefix-in ak: "abstract-knowledge.rkt"))
+(require (prefix-in ck: "concrete-knowledge.rkt"))
 
 (define (visit-from idx n)
   (match n
-    [(node (tree-label _ _ _ rule idx2 _) _)
-     (displayln (format "transition_from_to_via(~a,~a,~a)." idx idx2 rule))]
+    [(node (tree-label _ _ _ (ck:rule _ _ rule-idx) idx2 _) _)
+     (displayln (format "transition_from_to_via(~a,~a,rule~a)." idx idx2 rule-idx))]
+    [(node (tree-label _ _ _ (ak:full-evaluation _ _ rule-idx) idx2 _) _)
+     (displayln (format "transition_from_to_via(~a,~a,fullai~a)." idx idx2 rule-idx))]
     [(node (generalization _ _ _ _ _) _)
      (displayln (format "generalization_from_to()"))]
     [(node (cycle cycle-idx) _)
@@ -14,12 +18,13 @@
      (displayln "don't know how to be visited yet")]))
 
 (define (visitor n)
-  ;; TODO: match is not a great fit here
   (match n
     [(node (and (? label-with-conjunction?) label) children)
      (for ([ch children]) (visit-from (label-index label) ch))]
-    [_ (displayln "don't know how to visit yet")]))
+    [(node (cycle idx) (list)) (void)]
+    [_ (displayln (format "don't know how to visit ~a yet" n))]))
 
 (define (display-mi-map tree)
-  (visit visitor tree))
+  (visit visitor tree)
+  tree)
 (provide display-mi-map)
