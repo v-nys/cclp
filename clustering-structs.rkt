4951b77e505c92f556b3078826d0f3bd67656173
Vincent Nys <vincentnys@gmail.com>
2017-10-18 11:08:13 +0200

    Splitting out the clustering stuff. Would like to be able to see generations for each cluster in visualization.

---
+++ clustering-structs.rkt
@@ -0,0 +1,27 @@
+#lang at-exp racket
+(require
+  scribble/srcdoc
+  (for-doc scribble/manual)
+  cclp/gen-graph-structs)
+
+(struct clustering (subclusters gcd)
+  #:methods
+  gen:equal+hash
+  [(define (equal-proc a b equal?-recur)
+     ; compare a and b
+     (and (equal?-recur (clustering-subclusters a) (clustering-subclusters b))
+          (equal?-recur (clustering-gcd a) (clustering-gcd b))))
+   (define (hash-proc a hash-recur)
+     ; compute primary hash code of a
+     (+ (hash-recur (clustering-subclusters a))
+        (* 3 (hash-recur (clustering-gcd a)))))
+   (define (hash2-proc a hash2-recur)
+     ; compute secondary hash code of a
+     (+ (hash2-recur (clustering-subclusters a))
+        (hash2-recur (clustering-gcd a))))])
+(provide
+ (struct*-doc
+  clustering
+  ([subclusters (or/c (set/c clustering?) gen-node?)]
+   [gcd exact-positive-integer?])
+  @{A grouping of @racket[gen-node?] structs @racket[subclusters] that have a common ancestor encoded as @racket[gcd].}))
