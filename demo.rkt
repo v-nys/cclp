2cd73fd061077d1348bcd5a16ed307285cb95fb6
Vincent Nys <vincentnys@gmail.com>
2018-05-01 16:38:44 +0200

    Commit for the sake of reference.

--- demo.rkt
+++ demo.rkt
@@ -4,15 +4,19 @@
   cclp-analysis/interaction
   cclp-analysis/synthesis
   cclp-analysis/new-mi-map
-  cclp-programs/primes
-  ;cclp-programs/permutation-sort
+  cclp-programs/graph-coloring
+  positional-tree-utils
   positional-tree-utils/printer
   cclp-common/abstract-analysis)
 (define complete-analysis (apply↑* proceed initial-program-analysis))
-(define segments (segments-for-postprocessing ((compose sort-segments set->list synthesizable-segments analysis-tree) complete-analysis) (analysis-tree complete-analysis)))
-(define endpoints (map (λ (l) (cons (label-index (first l)) (last l))) segments))
-(for-each
- displayln
- endpoints)
-;(tree-display  print-tree-label)
+;(define segments
+;  (segments-for-postprocessing
+;   ((compose sort-segments set->list synthesizable-segments analysis-tree) complete-analysis)
+;   (analysis-tree complete-analysis)))
+;(define endpoints (map (λ (l) (cons (label-index (first l)) (last l))) segments))
+;(for-each
+; displayln
+; endpoints)
+
+(tree-display (analysis-tree complete-analysis) print-tree-label)
 ;(display-mi-map (analysis-tree complete-analysis))
